668ddf37c1bf8975dbdfb140fb11d590a3189b50
j.takubo <j.takubo@datumstudio.jp>
2021-01-14 21:55:25 +0900

    binary dataを送信出来るように修正

--- backend/comic_generator/views.py
+++ backend/comic_generator/views.py
@@ -16,7 +16,7 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 class manGANAPIView(views.APIView):
     serializer_class = manGANSerializer
@@ -25,35 +25,6 @@ class manGANAPIView(views.APIView):
     
     def get(self, request, format=None):
         return send_image(request)
-    def dispatch(self, request, *args, **kwargs):
-        """
-        `.dispatch()` is pretty much the same as Django's regular dispatch,
-        but with extra hooks for startup, finalize, and exception handling.
-        """
-        self.args = args
-        self.kwargs = kwargs
-        request = self.initialize_request(request, *args, **kwargs)
-        self.request = request
-        self.headers = self.default_response_headers  # deprecate?
-
-        try:
-            self.initial(request, *args, **kwargs)
-            # send_image(request)
-
-            # Get the appropriate handler method
-            if request.method.lower() in self.http_method_names:
-                handler = getattr(self, request.method.lower(),
-                                  self.http_method_not_allowed)
-            else:
-                handler = self.http_method_not_allowed
-
-            response = handler(request, *args, **kwargs)
-
-        except Exception as exc:
-            response = self.handle_exception(exc)
-        self.response = self.finalize_response(request, response, *args, **kwargs)
-        return self.response
-
 
 
 
@@ -62,7 +33,7 @@ def send_image(request):
     # 上記コマンドでraspberry piから送信
     # https://stackoverflow.com/questions/47515243/reading-image-file-file-storage-object-using-cv2
     # read methodが必要
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     try:
          # image file
         expected_datatype = (django.core.files.uploadedfile.InMemoryUploadedFile, django.core.files.uploadedfile.TemporaryUploadedFile)
@@ -75,7 +46,7 @@ def send_image(request):
 
         # 2. prediction
         # generated_img = manGAN.predict(img_array)
-        img = img - 0.5
+        # img = img - 0.5
 
         # 3. convert the generated_img to binary format
         # https://chiyoh.hatenablog.com/entry/2019/05/04/145639
@@ -84,7 +55,7 @@ def send_image(request):
         output = BytesIO()
         bin_img = Image.fromarray(img, 'RGB')
         bin_img.save(output, format='JPEG')
-        output.getvalue()
+        
         
 
         # 4. create JSONREsponse
@@ -98,5 +69,5 @@ def send_image(request):
         # JSONResponse()
     except Exception as exc:
         raise ImageHandlingError(exc)
-    return JsonResponse({"content":str(output.getvalue())})
+    return HttpResponse(output.getvalue(), content_type="image/png")
 
