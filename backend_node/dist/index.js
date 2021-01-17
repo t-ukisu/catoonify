88c8edd88ef29c248ade1c4da1d97f8f82fae4ef
j.takubo <j.takubo@datumstudio.jp>
2021-01-18 00:02:58 +0900

    [WIP]node-jsでAPI作ってonnx.js使ってみようと試みる

--- backend_node/dist/index.js
+++ backend_node/dist/index.js
@@ -4,26 +4,35 @@ const onnx = require('onnxjs');
 // import { Tensor, InferenceSession } from 
 const Tensor = onnx.Tensor;
 const InferenceSession = onnx.InferenceSession;
-const hostname = "127.0.0.1";
-const port = 8000;
-function hello(name) {
-    return `Hello, ${name}!`;
+// ------------
+const http = require('http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.post('/', (req, res) => {
+    res.send('Simple REST API');
+});
+// ------------
+// function hello(name: string): string {
+//   return `Hello, ${name}!`;
+// }
+// console.log(hello("World"));
+async function runModel(input_array_) {
+    debugger;
+    // 推論に用いるセッションの初期化
+    // Backendには cpu や webgl, wasm を利用することができます
+    const session = new InferenceSession({ backendHint: 'webgl' });
+    // ONNX形式のモデルファイル
+    const modelFile = './sample.onnx'; // ここで詰まる................................................................                                   
+    // モデルの読み込み
+    await session.loadModel(modelFile);
+    // Inputデータの準備（必要であれば事前に前処理をしておく必要があります）
+    // とりあえず、すべて0とするダミーデータを用意します
+    const inputTensor = new Tensor(input_array_, 'float32', [1, 3, 450, 450]);
+    // 推論の実行
+    const outputData = await session.run([inputTensor]);
+    return outputData;
 }
-console.log(hello("World"));
-// async runModel(input_array_:any) {
-//     debugger;
-//     // 推論に用いるセッションの初期化
-//     // Backendには cpu や webgl, wasm を利用することができます
-//     const session = new InferenceSession({ backendHint: 'webgl' })
-//     // ONNX形式のモデルファイル
-//     const modelFile = './sample.onnx' // ここで詰まる................................................................                                   
-//     // モデルの読み込み
-//     await session.loadModel(modelFile)
-//     // Inputデータの準備（必要であれば事前に前処理をしておく必要があります）
-//     // とりあえず、すべて0とするダミーデータを用意します
-//     const inputTensor = new Tensor(input_array_, 'float32', [1, 3, 450, 450])
-//     // 推論の実行
-//     const outputData = await session.run([inputTensor])
-//     return outputData;
-//   };
+;
 //# sourceMappingURL=index.js.map
--- backend_node/index.ts
+++ backend_node/index.ts
@@ -4,35 +4,46 @@ const onnx = require('onnxjs');
 const Tensor = onnx.Tensor
 
 const InferenceSession = onnx.InferenceSession
-const hostname = "127.0.0.1";
-const port = 8000;
+// ------------
+
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.post('/', (req:any, res:any) => {
+  res.send('Simple REST API');
+});
 
 
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+// ------------
+// function hello(name: string): string {
+//   return `Hello, ${name}!`;
+// }
 
-console.log(hello("World"));
+// console.log(hello("World"));
 
 
 
-// async runModel(input_array_:any) {
-//     debugger;
-//     // 推論に用いるセッションの初期化
-//     // Backendには cpu や webgl, wasm を利用することができます
-//     const session = new InferenceSession({ backendHint: 'webgl' })
+async function runModel(input_array_:any) {
+    debugger;
+    // 推論に用いるセッションの初期化
+    // Backendには cpu や webgl, wasm を利用することができます
+    const session = new InferenceSession({ backendHint: 'webgl' })
 
-//     // ONNX形式のモデルファイル
-//     const modelFile = './sample.onnx' // ここで詰まる................................................................                                   
+    // ONNX形式のモデルファイル
+    const modelFile = './sample.onnx' // ここで詰まる................................................................                                   
 
-//     // モデルの読み込み
-//     await session.loadModel(modelFile)
+    // モデルの読み込み
+    await session.loadModel(modelFile)
 
-//     // Inputデータの準備（必要であれば事前に前処理をしておく必要があります）
-//     // とりあえず、すべて0とするダミーデータを用意します
-//     const inputTensor = new Tensor(input_array_, 'float32', [1, 3, 450, 450])
+    // Inputデータの準備（必要であれば事前に前処理をしておく必要があります）
+    // とりあえず、すべて0とするダミーデータを用意します
+    const inputTensor = new Tensor(input_array_, 'float32', [1, 3, 450, 450])
 
-//     // 推論の実行
-//     const outputData = await session.run([inputTensor])
-//     return outputData;
-//   };
+    // 推論の実行
+    const outputData = await session.run([inputTensor])
+    return outputData;
+  };
